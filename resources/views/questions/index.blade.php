0887fad0d8b12ef09b50ea61ab306df4411889cc
Himanshu Raval <raval_himanshu@live.in>
2020-08-04 16:21:55 +0530

    Added policies

--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -35,13 +35,16 @@
                                         <a href="{{ $question->url }}">{{ $question->title }}</a>
                                     </h3>
                                     <div class="ml-auto">
+                                        @can('update',$question)
                                         <a href="{{ route('questions.edit', $question->id) }}" class="btn btn-sm btn-outline-info">Edit</a>
-
+                                        @endcan
+                                        @can('delete',$question)
                                         <form action="{{ route('questions.destroy', $question->id) }}" method="POST" class="form-delete">
                                             {{ method_field('DELETE') }}
                                             {{ csrf_field() }}
                                             <button type="sibmit" class="btn btn-sm btn-outline-danger" onclick="return confirm('Are you sure?')">Delete</button>
                                         </form>
+                                        @endcan
                                     </div>
                                 </div>
                                 
