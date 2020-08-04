56a7a7365fba92d1b87366cc6aa05013f25057f5
Himanshu Raval <raval_himanshu@live.in>
2020-08-04 17:25:36 +0530

    Generated fake answers

--- app/Answer.php
+++ app/Answer.php
@@ -8,16 +8,27 @@ class Answer extends Model
 {
    public function question()
    {
-   		$this->belongsTo(Question::class);
+   		return $this->belongsTo(Question::class);
    }
 
    public function user()
    {
-   		$this->belongsTo(User::class);
+   		return $this->belongsTo(User::class);
    }
 
    public function getBodyHtmlAttribute()
     {
         return \Parsedown::instance()->text($this->body);
+    }
+
+    public static function boot($value='')
+    {
+    	parent::boot();
+
+    	static::created(function($answer){
+    		$answer->question->increment('answers_count');
+    		$answer->question->save();
+    	});
+
     }
 }
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -15,7 +15,10 @@ class DatabaseSeeder extends Seeder
         	$user->questions()
         	  ->saveMany(
         	  	factory(App\Question::class, rand(1,5))->make()
-        	  );
+        	  )
+              ->each(function($question){
+                $question->answers()->saveMany(factory(App\Answer::class, rand(1, 5))->make());
+              });
         });
     }
 }
