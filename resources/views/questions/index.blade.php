7661f30a7568835f7a21451f56eaa2bda4791306
Himanshu Raval <raval_himanshu@live.in>
2020-08-04 15:33:49 +0530

    Added delete functionality

--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -36,6 +36,12 @@
                                     </h3>
                                     <div class="ml-auto">
                                         <a href="{{ route('questions.edit', $question->id) }}" class="btn btn-sm btn-outline-info">Edit</a>
+
+                                        <form action="{{ route('questions.destroy', $question->id) }}" method="POST" class="form-delete">
+                                            {{ method_field('DELETE') }}
+                                            {{ csrf_field() }}
+                                            <button type="sibmit" class="btn btn-sm btn-outline-danger" onclick="return confirm('Are you sure?')">Delete</button>
+                                        </form>
                                     </div>
                                 </div>
                                 
