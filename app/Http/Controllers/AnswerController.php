7c26f0c318d6cfb0ab606073122a642ea2c7de2b
Himanshu Raval <raval_himanshu@live.in>
2020-08-05 17:31:22 +0530

    Update answer

--- app/Http/Controllers/AnswerController.php
+++ app/Http/Controllers/AnswerController.php
@@ -31,9 +31,11 @@ class AnswerController extends Controller
      * @param  \App\Answer $answer
      * @return \Illuminate\Http\Response
      */
-    public function edit(Answer $answer)
+    public function edit(Question $question, Answer $answer)
     {
-        //
+        $this->authorize('update',$answer);
+
+        return view('answers._edit', compact('answer','question'));
     }
 
     /**
@@ -43,9 +45,15 @@ class AnswerController extends Controller
      * @param  \App\Answer $answer
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Answer $answer)
+    public function update(Request $request, Question $question, Answer $answer)
     {
-        //
+        $this->authorize('update',$answer);
+
+        $answer->update($request->validate([
+            'body'  =>  'required'
+        ]));
+
+        return redirect()->route('questions.show', $question->slug)->with('success', 'Your answer has been updated.');
     }
 
     /**
@@ -54,8 +62,8 @@ class AnswerController extends Controller
      * @param  \App\Answer $answer
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Answer $answer)
+    public function destroy(Question $question, Answer $answer)
     {
-        //
+        $this->authorize('delete',$answer);
     }
 }
