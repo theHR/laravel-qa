f37a60c6449314c8b08484b77bd82e3ff13d655c
Himanshu Raval <raval_himanshu@live.in>
2020-08-05 17:10:26 +0530

    Display answer for questions

--- app/Answer.php
+++ app/Answer.php
@@ -6,34 +6,36 @@ use Illuminate\Database\Eloquent\Model;
 
 class Answer extends Model
 {
-   public function question()
-   {
-   		return $this->belongsTo(Question::class);
-   }
+  protected $fillable = ['body','user_id'];
 
-   public function user()
-   {
-   		return $this->belongsTo(User::class);
-   }
+  public function question()
+  {
+  		return $this->belongsTo(Question::class);
+  }
 
-   public function getBodyHtmlAttribute()
-    {
-        return \Parsedown::instance()->text($this->body);
-    }
+  public function user()
+  {
+  		return $this->belongsTo(User::class);
+  }
 
-    public static function boot($value='')
-    {
-    	parent::boot();
+  public function getBodyHtmlAttribute()
+  {
+      return \Parsedown::instance()->text($this->body);
+  }
 
-    	static::created(function($answer){
-    		$answer->question->increment('answers_count');
-    		$answer->question->save();
-    	});
+  public static function boot($value='')
+  {
+  	parent::boot();
 
-    }
+  	static::created(function($answer){
+  		$answer->question->increment('answers_count');
+  		$answer->question->save();
+  	});
 
-    public function getCreatedDateAttribute()
-    {
-        return $this->created_at->diffForHumans();
-    }
+  }
+
+  public function getCreatedDateAttribute()
+  {
+      return $this->created_at->diffForHumans();
+  }
 }
