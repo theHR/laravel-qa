2c311980462c7d616b60732e4c93553db6610901
Himanshu Raval <raval_himanshu@live.in>
2020-08-10 18:59:23 +0530

    Securing for xss

--- app/Question.php
+++ app/Question.php
@@ -22,6 +22,11 @@ class Question extends Model
     	$this->attributes['slug'] = Str::slug($value);
     }
 
+    // public function setBodyAttribute($value)
+    // {
+    //     $this->attributes['body'] = clean($value);
+    // }
+
     public function getUrlAttribute()
     {
         return route('questions.show', $this->slug);
@@ -49,7 +54,7 @@ class Question extends Model
 
     public function getBodyHtmlAttribute()
     {
-        return \Parsedown::instance()->text($this->body);
+        return clean($this->bodyHtml());
     }
 
     public function answers()
@@ -83,4 +88,18 @@ class Question extends Model
         return $this->favorites()->count();   
     }
 
+    public function getExcerptAttribute()
+    {
+        return $this->excerpt(250);
+    }
+
+    public function excerpt($length)
+    {
+        return \Str::limit(strip_tags($this->bodyHtml()), $length);
+    }
+
+    private function bodyHtml()
+    {
+        return \Parsedown::instance()->text($this->body);
+    }
 }
