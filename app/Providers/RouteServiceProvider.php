a05d323b7412c942bafb76964bceb9c7ad345e15
Himanshu Raval <raval_himanshu@live.in>
2020-08-10 19:13:24 +0530

    Misc fixes

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -31,6 +31,9 @@ class RouteServiceProvider extends ServiceProvider
     public function boot()
     {
         Route::bind('slug', function($slug){
+            // return \App\Question::with(['answers.user','answers' => function($query){ 
+            //         $query->orderBy('votes_count', 'DESC');
+            //  }])->where('slug', $slug)->first() ?? abort(404);
             return \App\Question::with('answers.user')->where('slug', $slug)->first() ?? abort(404);
         });
 
