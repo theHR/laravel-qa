b88278e42e378616c56a7a49b63bdcf13b23aa6c
Himanshu Raval <raval_himanshu@live.in>
2020-08-10 15:55:03 +0530

    Implemented Voting

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,7 +13,8 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             'UsersQiestionsAnswersSeeder',
-            'FavoritesTableSeeder'
+            'FavoritesTableSeeder',
+            'VotablesTableSeeder'
         ]);
     }
 }
